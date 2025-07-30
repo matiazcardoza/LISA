b340f0b6cbc0fe36a76cee8192de56868dab9e84
ROYER MATIAZ HUANCA CARDOZA <rohuancaca@est.unap.edu.pe>
2025-07-29 20:58:08 -0500

    este es el commit del proyecto

---
+++ frontend/src/App.vue
@@ -0,0 +1,19 @@
+<script setup>
+import { onMounted, ref } from 'vue'
+import api from './axios'
+
+const mensaje = ref('Cargando...')
+
+onMounted(async () => {
+  try {
+    const res = await api.get('/ping')
+    mensaje.value = res.data.message
+  } catch (err) {
+    mensaje.value = 'Error de conexión con el backend'
+  }
+})
+</script>
+
+<template>
+  <h1>{{ mensaje }}</h1>
+</template>
